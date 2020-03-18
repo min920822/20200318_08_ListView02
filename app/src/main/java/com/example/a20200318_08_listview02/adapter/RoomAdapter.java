d0d52ce7dee78a8fa127c6bfb8a44964bbccc6c3
min920822 <kings789@naver.com>
2020-03-18 15:19:48 +0900

    RoomAdapter 클래스 기초 작업

---
+++ app/src/main/java/com/example/a20200318_08_listview02/adapter/RoomAdapter.java
@@ -0,0 +1,44 @@
+package com.example.a20200318_08_listview02.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.a20200318_08_listview02.R;
+import com.example.a20200318_08_listview02.datas.Room;
+
+import java.util.List;
+
+public class RoomAdapter extends ArrayAdapter<Room> {
+
+    Context mContext;
+    List<Room> mList;
+    LayoutInflater inf;
+
+    public RoomAdapter(@NonNull Context context, int resource, @NonNull List<Room> objects) {
+        super(context, resource, objects);
+
+        mContext = context;
+        mList = objects;
+        inf = LayoutInflater.from(mContext);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+        if (row == null){
+            row = inf.inflate(R.layout.room_list_item,null);
+        }
+
+        return row;
+
+
+    }
+}
