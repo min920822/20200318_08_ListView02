4e4b3adff28840a500ff96ebc4462954523030e2
min920822 <kings789@naver.com>
2020-03-18 14:51:36 +0900

    방 목록 생성

---
+++ app/src/main/java/com/example/a20200318_08_listview02/datas/Room.java
@@ -0,0 +1,50 @@
+package com.example.a20200318_08_listview02.datas;
+
+import java.io.Serializable;
+
+public class Room implements Serializable {
+
+    private int price;
+    private String address;
+    private int floor;
+    private String description;
+
+    public Room(int price, String address, int floor, String description) {
+        this.price = price;
+        this.address = address;
+        this.floor = floor;
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
